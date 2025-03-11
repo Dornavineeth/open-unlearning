728f58b60d17fc89f6c0ebe71e558f5911a53ab6
molereddy <m.anmolreddy@gmail.com>
2025-03-11 19:51:42 +0000

    Simplified privleak version

--- src/evals/metrics/privacy.py
+++ src/evals/metrics/privacy.py
@@ -1,7 +1,6 @@
 import numpy as np
 from scipy.stats import ks_2samp
 from torch.utils.data import DataLoader
-from sklearn.metrics import auc as get_auc, roc_curve as get_roc_curve
 
 from evals.metrics.base import unlearning_metric, logger
 from evals.metrics.utils import run_batchwise_evals, tokenwise_logprobs
@@ -70,49 +69,7 @@ def minKpc_negative_logprob(model, **kwargs):
 
 @unlearning_metric(name="privleak")
 def privleak(model, **kwargs): # re-name to privleak
-    """Compute the auc score of an MIA attack wrt model scores on a victim and holdout set"""
-
-    def sweep(ppl, y):
-        fpr, tpr, _ = get_roc_curve(y, -ppl)
-        acc = np.max(1 - (fpr + (1 - tpr)) / 2)
-        return fpr, tpr, get_auc(fpr, tpr), acc
-
-    forget_scores = kwargs["pre_compute"]["forget"]["value_by_index"].values()
-    forget_scores = [elem["score"] for elem in forget_scores]
-    forget_holdout_scores = kwargs["pre_compute"]["holdout"]["value_by_index"].values()
-    forget_holdout_scores = [elem["score"] for elem in forget_holdout_scores]
-    scores = np.array(forget_scores + forget_holdout_scores)
-    # in MUSE the scores are -mean(min k% log-probs) for some reason so flip the 1 and 0
-    labels = np.array([0] * len(forget_scores) + [1] * len(forget_holdout_scores))
-
-    _, _, auc_score, acc = sweep(scores, labels)
-
-    output = {
-        "acc": acc,
-        "auc": auc_score,
-    }
-    retain_auc_score = kwargs["ref_value"]
-
-    reference_logs = kwargs.get("reference_logs", None)
-    if reference_logs:
-        retain_scores = reference_logs["retain_model_logs"]["retain"][
-            "value_by_index"
-        ].values()
-        retain_scores = [elem["score"] for elem in retain_scores]
-        retain_holdout_scores = reference_logs["retain_model_logs"]["holdout"][
-            "value_by_index"
-        ].values()
-        retain_holdout_scores = [elem["score"] for elem in retain_holdout_scores]
-        scores = np.array(retain_scores + retain_holdout_scores)
-        labels = np.array([0] * len(retain_scores) + [1] * len(retain_holdout_scores))
-        _, _, retain_auc_score, retain_acc = sweep(scores, labels)
-        output.update({"retain_acc": retain_acc, "retain_auc_score": retain_auc_score})
-
-    output.update(
-        {
-            "agg_value": (auc_score - retain_auc_score)
-            / (retain_auc_score)
-            * 100  # privleak score in muse
-        }
-    )
-    return output
+    """Compares a statistic computed on a model to a reference retain model"""
+    score = kwargs["pre_compute"]["target_model_stat"]["agg_value"]
+    ref = kwargs["pre_compute"]["ref_model_stat"]["agg_value"]
+    return {'agg_value': (score-ref)/ref*100}
