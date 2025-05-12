8273517803dcb274bf1d3103fcd68c62ec2c1088
Dornavineeth <vineethdorna@gmail.com>
2025-05-12 19:15:02 +0000

    ruff fix

--- setup_data.py
+++ setup_data.py
@@ -3,6 +3,7 @@ import os
 import subprocess
 from huggingface_hub import snapshot_download
 
+
 def download_eval_data():
     snapshot_download(
         repo_id="open-unlearning/eval",
@@ -11,6 +12,7 @@ def download_eval_data():
         local_dir="saves/eval",
     )
 
+
 def download_idk_data():
     snapshot_download(
         repo_id="open-unlearning/idk",
@@ -18,6 +20,7 @@ def download_idk_data():
         repo_type="dataset",
         local_dir="data",
     )
+
 
 def download_wmdp():
     url = "https://cais-wmdp.s3.us-west-1.amazonaws.com/wmdp-corpora.zip"
@@ -28,11 +31,24 @@ def download_wmdp():
     subprocess.run(["wget", url, "-O", zip_path], check=True)
     subprocess.run(["unzip", "-P", "wmdpcorpora", zip_path, "-d", dest_dir], check=True)
 
+
 def main():
     parser = argparse.ArgumentParser(description="Download and setup evaluation data.")
-    parser.add_argument("--eval_logs", action="store_true", help="Downloads TOFU, MUSE  - retain and finetuned models eval logs and saves them in saves/eval")
-    parser.add_argument("--idk", action="store_true", help="Download idk dataset from HF hub and stores it data/idk.jsonl")
-    parser.add_argument("--wmdp", action="store_true", help="Download and unzip WMDP dataset into data/wmdp")
+    parser.add_argument(
+        "--eval_logs",
+        action="store_true",
+        help="Downloads TOFU, MUSE  - retain and finetuned models eval logs and saves them in saves/eval",
+    )
+    parser.add_argument(
+        "--idk",
+        action="store_true",
+        help="Download idk dataset from HF hub and stores it data/idk.jsonl",
+    )
+    parser.add_argument(
+        "--wmdp",
+        action="store_true",
+        help="Download and unzip WMDP dataset into data/wmdp",
+    )
 
     args = parser.parse_args()
 
@@ -42,6 +58,7 @@ def main():
         download_idk_data()
     if args.download_wmdp:
         download_wmdp()
+
 
 if __name__ == "__main__":
     main()
